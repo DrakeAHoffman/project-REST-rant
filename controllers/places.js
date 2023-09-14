0c8e0a0186ae8b148fd893d3ec718e3c063df648
DrakeAHoffman <drakehoffman99@gmail.com>
2023-09-13 19:26:09 -0700

    part 11

--- controllers/places.js
+++ controllers/places.js
@@ -89,12 +89,29 @@ router.put('/:id', (req, res) => {
 
 
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
+
+
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
+
+
 
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
@@ -124,6 +141,19 @@ router.post('/:id/comment', (req, res) => {
       res.render('error404')
   })
 })
+
+router.put('/:id', (req, res) => {
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+
 
 
 
